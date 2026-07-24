f4d302def74e8ff4e7ceaa374f15e0360361ca27
FoxtrotMagni <foxtrotmagni@gmail.com>
2026-07-24 10:29:55 +0700

    Initial commit - Bot & Portal WA Update

---
+++ JALANKAN_BOT.bat
@@ -0,0 +1,9 @@
+@echo off
+title Telegram Bot & Web Server Runner
+echo ========================================================
+echo   MEMULAI BOT TELEGRAM STANDALONE (NODE.JS)
+echo ========================================================
+echo.
+cd /d "%~dp0"
+node server.js
+pause
